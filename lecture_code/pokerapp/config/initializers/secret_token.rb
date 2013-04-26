b0d921fbc10e688c345e104ef1709743db562655
Mattocks Swenson <kmattocks.swenson@gmail.com>
2013-04-26 16:45:51 -0500

    Week 3 Homework completed

---
+++ lecture_code/pokerapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pokerapp::Application.config.secret_token = 'e6ae7710ec79d749642ae9d1ec7f33c6cb986550e9d5961fa49c784642e21d89aee6a5247db6b26fe4d4c94b27dcea30351e378939ffd0c708eef89abb747f80'
